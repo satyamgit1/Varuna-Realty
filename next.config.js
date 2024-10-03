bfbd9d73cd3c1cdf2b16de3d531d4a3192421aa7
Satyam singh <satyamsingh2003a@gmail.com>
2024-10-03 16:20:21 +0530

    PWA-to_progressive web app

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const withPWA = require('next-pwa')({
   dest: 'public', // the destination where PWA files will be generated
-  swSrc: 'public/sw.js', // Custom service worker
   disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
 });
 
