6d7222ebdccc03a2effd5982dfbda679a09a6dda
Satyam singh <satyamsingh2003a@gmail.com>
2024-10-03 14:49:50 +0530

    PWA-to_progressive web app

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  });
-  
-  module.exports = withBundleAnalyzer({
-    // Other Next.js config options
-  });
-  
+  enabled: process.env.ANALYZE === 'true',
+});
+const withPWA = require('next-pwa')({
+  dest: 'public', // destination of service worker and other PWA-related files
+});
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    // Place any other Next.js configuration options here
+    reactStrictMode: true, // Example configuration
+  })
+);
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to the manifest file */}
+        <link rel="manifest" href="/manifest.json" />
+
+        
+        {/* PWA icons */}
+        <link rel="apple-touch-icon" href="/coffee.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/coffee.png" />
+        
+        {/* Meta tags for theme color */}
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
