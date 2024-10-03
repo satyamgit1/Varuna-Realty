c6ede90615806b719fcc197639574e4fb42cc146
Satyam singh <satyamsingh2003a@gmail.com>
2024-10-03 15:04:02 +0530

    Created PWA-App

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
+// next.config.js
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+
 const withPWA = require('next-pwa')({
-  dest: 'public', // destination of service worker and other PWA-related files
+  dest: 'public', // the destination where PWA files will be generated
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
 });
 
 module.exports = withBundleAnalyzer(
   withPWA({
-    // Place any other Next.js configuration options here
-    reactStrictMode: true, // Example configuration
+    reactStrictMode: true, // Other Next.js config options
   })
 );
